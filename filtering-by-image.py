946e7ab18af349d5838a588c6252a67090fb781d
dien <diennguyenduy1311999@gmail.com>
2020-05-24 23:53:16 +0700

    add filter

--- filtering-by-image.py
+++ filtering-by-image.py
@@ -1,12 +1,46 @@
 import cv2
 import time
 import numpy as np
+import scipy.ndimage
 from scipy.interpolate import UnivariateSpline
 
 
 def spreadLookupTable(x, y):
     spline = UnivariateSpline(x, y)
     return spline(range(256))
+
+
+def grayscale(rgb):
+    return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
+
+
+def dodge(front, back):
+    result = front*255/(255-(back-1))
+    result[result > 255] = 255
+    result[back == 255] = 255
+    return result.astype('uint8')
+
+
+def sobel(img):
+    opImgx = cv2.Sobel(img, cv2.CV_8U, 0, 1, ksize=-
+                       1)  # detects horizontal edges
+    opImgy = cv2.Sobel(img, cv2.CV_8U, 1, 0, ksize=-
+                       1)  # detects vertical edges
+    # combine both edges
+    return cv2.bitwise_or(opImgx, opImgy)
+
+
+def resize(image, window_height=500):
+    aspect_ratio = float(image.shape[1])/float(image.shape[0])
+    window_width = window_height/aspect_ratio
+    image = cv2.resize(image, (int(window_height), int(window_width)))
+    return image
+
+
+def alpha_blend(frame_1, frame_2, mask):
+    alpha = mask/255.0
+    blended = cv2.convertScaleAbs(frame_1*(1-alpha) + frame_2*alpha)
+    return blended
 
 
 path = (input("Enter the Path of File:"))
@@ -19,6 +53,15 @@ dict_1 = {
     4: 'Stylization Filter',
     5: 'Sepia Filter',
     6: 'Warming Filter',
+    7: 'Convert to sketch',
+    8: 'Saturation',
+    9: 'Green',
+    10: 'Cartoon image',
+    11: 'Pencil',
+    12: 'Snow',
+    13: 'Laplacian',
+    14: 'Bilateral',
+    15: 'Value channel'
 }
 
 height = img.shape[0]
@@ -79,15 +122,100 @@ while(True):
 
         cv2.imshow("Warming Filter", replaced_image)
         img1 = replaced_image
-    # elif num == '7':
-    # elif num == '8':
-    # elif num == '9':
-    # elif num == '10':
-    # elif num == '11':
-    # elif num == '12':
-    # elif num == '13':
-    # elif num == '14':
-    # elif num == '15':
+    elif num == '7':
+        g = grayscale(img)
+        i = 255-g
+
+        b = scipy.ndimage.filters.gaussian_filter(i, sigma=3)
+        r = dodge(b, g)
+
+        edges = cv2.Canny(img, 100, 200)
+        contours, hierarchy = cv2.findContours(
+            edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        img_clone = np.zeros_like(edges)
+        for cnt in contours:
+            size = cv2.contourArea(cnt)
+            if size > 10:
+                cv2.drawContours(r, [cnt], -1, (200, 0, 50), 1)
+
+        cv2.imshow('Sketch', cv2.cvtColor(r, cv2.COLOR_BGR2RGB))
+    elif num == '8':
+        img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        cv2.imshow("Saturation", img_HSV[:, :, 1])
+        img1 = img_HSV[:, :, 1]
+    elif num == '9':
+        B, G, R = cv2.split(img)
+        zeros = np.zeros((height, width), dtype="uint8")
+        cv2.imshow("Green", cv2.merge([zeros, G, zeros]))
+        img1 = cv2.merge([zeros, G, zeros])
+    elif num == '10':
+        img_color = img
+        for _ in range(2):
+            img_color = cv2.pyrDown(img_color)
+        for _ in range(5):
+            img_color = cv2.bilateralFilter(
+                img_color, d=9, sigmaColor=9, sigmaSpace=7)
+        # upsample image to original size
+        for _ in range(2):
+            img_color = cv2.pyrUp(img_color)
+        img_color = cv2.resize(img_color, img.shape[:2])
+        img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        img_blur = cv2.medianBlur(img_gray, 7)
+        img_edge = cv2.adaptiveThreshold(img_blur,
+                                         maxValue=255,
+                                         adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C,
+                                         thresholdType=cv2.THRESH_BINARY,
+                                         blockSize=9,
+                                         C=3)
+        img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
+        img_edge = cv2.resize(img_edge, img.shape[:2])
+        img_cartoon = cv2.bitwise_and(img_color, img_edge)
+        cv2.imshow('Cartoon image', img_cartoon[:, :, ::-1])
+    elif num == '11':
+        # Blur it to remove noise
+        frame = cv2.GaussianBlur(img, (3, 3), 0)
+        # make a negative image
+        invImg = 255-frame
+
+        # Detect edges from the input image and its negative
+        edgImg0 = sobel(frame)
+        edgImg1 = sobel(invImg)
+        # different weights can be tried too
+        edgImg = cv2.addWeighted(edgImg0, 1, edgImg1, 1, 0)
+
+        # Invert the image back
+        res = 255-edgImg
+        res = np.roll(res, res.shape[1]//2, axis=1)
+        img1 = res
+        cv2.imshow('Pencil', res)
+    elif num == '12':
+        image_HLS = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        # Conversion to HLS
+        image_HLS = np.array(image_HLS, dtype=np.float64)
+        brightness_coefficient = 2.5
+        snow_point = 100  # increase this for more snow
+        image_HLS[:, :, 1][image_HLS[:, :, 1] < snow_point] = image_HLS[:,
+                                                                        :, 1][image_HLS[:, :, 1] < snow_point]*brightness_coefficient
+        # scale pixel values up for channel 1(Lightness)
+        image_HLS[:, :, 1][image_HLS[:, :, 1] > 255] = 255
+        # Sets all values above 255 to 255
+        image_HLS = np.array(image_HLS, dtype=np.uint8)
+        image_RGB = cv2.cvtColor(
+            image_HLS, cv2.COLOR_HLS2RGB)  # Conversion to RGB
+        img1 = image_RGB
+        cv2.imshow("Snow", image_RGB)
+    elif num == '13':
+        new_image = cv2.Laplacian(img, cv2.CV_64F)
+        cv2.imshow('Laplacian', new_image)
+    elif num == '14':
+        # 9 ,75 and 75 are sigma color value and sigma space value affects cordinates space and color space
+        bilateral = cv2.bilateralFilter(img, 7, 20, 20)
+        cv2.imshow("Bilateral", bilateral)
+        img1 = bilateral
+    elif num == '15':
+        img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        cv2.imshow("Value channel", img_HSV[:, :, 2])
+        img1 = img_HSV[:, :, 2]
     else:
         print('invalid input!')
 
